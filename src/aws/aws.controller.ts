19ce054f2b2fc54d65718dd087a54d1ae63a9fbf
강승훈 <sunghun@gangseunghun-ui-MacBookAir.local>
2024-06-18 10:44:33 +0900

    ADD :: s3 file upload

--- src/aws/aws.controller.ts
+++ src/aws/aws.controller.ts
@@ -1,34 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { AwsService } from './aws.service';
-import { CreateAwDto } from './dto/create-aw.dto';
-import { UpdateAwDto } from './dto/update-aw.dto';
 
-@Controller('aws')
+import { FileInterceptor } from '@nestjs/platform-express';
+import { extname } from 'path';
+
+@Controller('s3')
 export class AwsController {
   constructor(private readonly awsService: AwsService) {}
 
   @Post()
-  create(@Body() createAwDto: CreateAwDto) {
-    return this.awsService.create(createAwDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.awsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.awsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAwDto: UpdateAwDto) {
-    return this.awsService.update(+id, updateAwDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.awsService.remove(+id);
+  @UseInterceptors(FileInterceptor('file'))
+  async create(@UploadedFile() file: Express.Multer.File): Promise<string> {
+    const ext = extname(file.originalname).substring(1);
+    const fileName = `${Date.now()}-${file.originalname}`;
+    return this.awsService.imageUploadToS3(fileName, file, ext);
   }
 }
--- src/aws/aws.service.ts
+++ src/aws/aws.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAwDto } from './dto/create-aw.dto';
 import { UpdateAwDto } from './dto/update-aw.dto';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AwsService {
-  create(createAwDto: CreateAwDto) {
-    return 'This action adds a new aw';
+  s3Client: S3Client;
+
+  constructor(private configService: ConfigService) {
+    this.s3Client = new S3Client({
+      region: this.configService.get('AWS_REGION'),
+      credentials: {
+        accessKeyId: this.configService.get('AWS_S3_ACCESS_KEY'), // Access Key
+        secretAccessKey: this.configService.get('AWS_S3_SECRET_ACCESS_KEY'), // Secret Key
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all aws`;
-  }
+  async imageUploadToS3(
+    fileName: string,
+    file: Express.Multer.File,
+    ext: string,
+  ) {
+    const command = new PutObjectCommand({
+      Bucket: this.configService.get('AWS_S3_BUCKET_NAME'),
+      Key: fileName,
+      Body: file.buffer,
+      ACL: 'public-read',
+      ContentType: `image/${ext}`,
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} aw`;
-  }
+    await this.s3Client.send(command);
 
-  update(id: number, updateAwDto: UpdateAwDto) {
-    return `This action updates a #${id} aw`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} aw`;
+    return `https://s3.${process.env.AWS_REGION}.amazonaws.com/${process.env.AWS_S3_BUCKET_NAME}/${fileName}`;
   }
 }
